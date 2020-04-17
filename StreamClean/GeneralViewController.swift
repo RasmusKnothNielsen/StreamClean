72356309d30ebcb7962f43963111d21b544d7bc8
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-04-17 15:27:43 +0200

    Added gitignore

---
+++ StreamClean/GeneralViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GeneralViewController.swift
+//  StreamClean
+//
+//  Created by Rasmus Nielsen on 17/04/2020.
+//  Copyright © 2020 Rasmus Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
